3aa37079640837ce6667890b9008fd3a09db85d6
Sameh reda <k5aleda7med@gmail.com>
2025-08-01 04:55:30 +0300

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBU4lTFTliRRmn4eN9F9pP9SKQJilUsXlE",
+    authDomain: "supplies-system.firebaseapp.com",
+    projectId: "supplies-system",
+    storageBucket: "supplies-system.appspot.com",
+    messagingSenderId: "205884975863",
+    appId: "1:205884975863:web:de65d32c2459f9979ac1f2",
+    measurementId: "G-2KSYJRKSCX"
+};
+
+export default firebaseConfig;
